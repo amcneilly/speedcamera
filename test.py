480ef817545e80a5b41cc351f85025856334b592
Adam McNeilly <amcneilly@gmail.com>
2024-06-07 13:16:10 +0000

    ss

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from tflite_runtime.interpreter import Interpreter
 # Configuration
 model_path = "ssd_mobilenet_v1_coco_quant_postprocess.tflite"  # Update with your model path
 labels_path = "coco_labels.txt"  # Update with your labels file path
-desired_object = "car"  # Object to detect
+desired_object = "dog"  # Object to detect
 recording_duration = 30  # Recording duration in seconds
 video_resolution = (1000, 600)  # Desired video resolution (width, height)
 model_input_size = (300, 300)  # Model input size (width, height)
